9cd09892f1f54c4bdca7b2aac95394137bd98cd8
Anand kushwaha <anandkushwaha70@gmail.com>
2022-07-27 02:48:24 +0530

    Favorite

--- movieapp/src/Components/Navbar.js
+++ movieapp/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <div style={{display:'flex',background:"lightyellow",padding:'1rem',justifyContent:"center",alignItems:"center"}}>
+      <div style={{display:'flex',background:"lightyellow  ",padding:'1rem',justifyContent:"center",alignItems:"center"}}>
         <h1>Movies App</h1>
         <h2 style={{marginLeft:"2rem",}}>Favorite</h2>
       </div>
